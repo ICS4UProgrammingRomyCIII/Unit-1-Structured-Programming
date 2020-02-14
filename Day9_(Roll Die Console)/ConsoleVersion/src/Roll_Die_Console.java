3e370c0ec5b576ac5493d99c88e47af9481b2902
Romy Chu III <s243855@ocsb.local>
2020-02-14 12:36:20 -0500

    Yes

---
+++ Day9_(Roll Die Console)/ConsoleVersion/src/Roll_Die_Console.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Roll_Die_Console {
+	static Scanner scan = new Scanner(System.in);
+	static final int MIN = 1;
+	static int maxNumber;
+	
+	public static void main(String[] args) {
+		GetUsrIn();
+	}
+	
+	public static void GetUsrIn() {
+		try {
+			System.out.print("Please type in a number.....");
+			String usrInput = scan.nextLine();
+			if (usrInput != null) {
+				maxNumber = Integer.parseInt(usrInput);
+			}
+			else {
+				return;
+			}
+			System.out.println(GenerateRandomNumber(maxNumber));
+		}
+		catch (Exception exc) {
+			System.out.println("Invalid Number");
+			GetUsrIn();
+		}
+	}
+	
+	
+	
+	public static int GenerateRandomNumber(int max) {
+		int randomNumber = 0;
+		Random ranNum = new Random();
+		randomNumber = ranNum.nextInt((max - MIN) + 1) + MIN;
+		return randomNumber;
+	}
+}
