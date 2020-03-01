233d2227f08f40d2e7b00ae6c92f0f763cb1221a
Romy Chu III <44402307+RC4398@users.noreply.github.com>
2020-03-01 14:44:02 -0500

    ...

--- Day8_(Dice Game)/GUIVersion/src/DiceGame.java
+++ Day8_(Dice Game)/GUIVersion/src/DiceGame.java
@@ -18,13 +18,16 @@ import org.eclipse.swt.events.SelectionEvent;
 
 
 public class DiceGame {
+	// These are variables for the objects that will be created later.
 	protected Shell frmDiceGame;
-	static final int MIN = 1;
-	static final int MAX = 6;
-	static int guessesSoFar = 0;
-	Random ranNum = new Random();
+	
+	Random ranNum = new Random();				  // Random object to generate a random int
+	static final int MIN = 1;				  // Variable for the minimum random value.
+	static final int MAX = 6;				  // Variable for the maximum random value.
+	static int guessesSoFar = 0;				  // Variable to track the total number of guesses.
 	int randomNumber = ranNum.nextInt((MAX - MIN) + 1) + MIN;
 	
+	//Automatically Created...
 	public static void main(String[] args) {
 		try {
 			DiceGame window = new DiceGame();
@@ -34,6 +37,7 @@ public class DiceGame {
 		}
 	}
 
+	//Automatically Created...
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
@@ -46,6 +50,7 @@ public class DiceGame {
 		}
 	}
 
+	//Automatically Created...
 	protected void createContents() {
 		frmDiceGame = new Shell();
 		frmDiceGame.setSize(450, 144);
@@ -69,24 +74,26 @@ public class DiceGame {
 		lblCorrectNumber.setText("The Correct Number is : ");
 		
 		Button btnGuess = new Button(frmDiceGame, SWT.NONE);
+		// This Block of code creates an action listener and attaches it to the button. 
 		btnGuess.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
-				CheckNumber(lblGuessesSoFar, lblCorrectNumber, nudGuess);
+				// When the button is pressed it calls CheckNumber() passing lblGuessesSoFar, lblCorrectNumber, and nudGuess as arguments.
+				CheckNumber(lblGuessesSoFar, lblCorrectNumber, nudGuess); 
 			}
 		});
 		btnGuess.setBounds(10, 51, 75, 25);
 		btnGuess.setText("Guess");
 	}
 	
+	// This procedure checks whether or not the user's guess is the same as the randomly generated number.
 	public void CheckNumber(Label lblGuesses, Label lblAns, Spinner nud) {
-		guessesSoFar++;
-		
-		int guess = Integer.parseInt(nud.getText());
-		if (guess == randomNumber) {
-			lblAns.setText("The Correct Number is : " + randomNumber);
+		guessesSoFar++;							  // When the procedure is called, the number of guesses increases.
+		int guess = Integer.parseInt(nud.getText());			  // Gets the user's guess from the numeric up-down box.
+		if (guess == randomNumber) {					  // If the user guesses correctly...
+			lblAns.setText("The Correct Number is : " + randomNumber);// Change the answer label to tell the user.
 		}
-		else {
-			lblGuesses.setText("Guesses So Far : " + guessesSoFar);
+		else {								  // Otherwise...
+			lblGuesses.setText("Guesses So Far : " + guessesSoFar);   // Increase the user's amount of guesses.
 		}
 	}
 }
