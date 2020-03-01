40729b5e3ff3bbff73e0b2f5fdd7ad16636e9867
Romy Chu III <44402307+RC4398@users.noreply.github.com>
2020-03-01 15:15:43 -0500

    ...

--- Day10_(Rock, Paper, Scissors)/GUIVersion/src/RockPaperScissors.java
+++ Day10_(Rock, Paper, Scissors)/GUIVersion/src/RockPaperScissors.java
@@ -18,19 +18,22 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 
 public class RockPaperScissors {
+	// These are variables for the objects that will be created later.
 	protected Shell frmRockPaperScissor;
 	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
-	static final int INT_RANGE = 3; //0 == Rock, 1 == Paper, 2 == Scissor. Random() is Exclusive (incl 0, excl last number).
-	static int playerWins = 0;
-	static int compWins = 0;
-	static String tie = "It was a Tie!!";
-	static String pWin = "The Player won!!";
-	static String cWin = "The Computer won!!";
-	
 	public Button btnCompRock;
 	public Button btnCompPaper;
 	public Button btnCompScissor;
 	
+	static final int INT_RANGE = 3; 	  // 0 == Rock, 1 == Paper, 2 == Scissor. Random() is Exclusive (incl 0, excl last number).
+	static int playerWins = 0;		  // A counter for the player's total wins.	
+	static int compWins = 0;		  // A counter for the computer's total wins.
+	static String tie = "It was a Tie!!";     // A string to output when there is a tie.
+	static String pWin = "The Player won!!";  // A string to output when the player wins.
+	static String cWin = "The Computer won!!";// A string to output when the computer wins.
+	
+
+	// Automatically Created...
 	public static void main(String[] args) {
 		try {
 			RockPaperScissors window = new RockPaperScissors();
@@ -39,6 +42,7 @@ public class RockPaperScissors {
 			e.printStackTrace();
 		}
 	}
+	// Automatically Created...
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
@@ -51,6 +55,7 @@ public class RockPaperScissors {
 		}
 	}
 
+	// Automatically Created...
 	protected void createContents() {
 		frmRockPaperScissor = new Shell();
 		frmRockPaperScissor.setSize(270, 239);
@@ -128,46 +133,47 @@ public class RockPaperScissors {
 		lblUsrChoice.setText("User's Choice : ");
 		
 		Button btnCheck = new Button(frmRockPaperScissor, SWT.NONE);
+		//This Block of code creates an action listener and attaches it to the button. 
 		btnCheck.addSelectionListener(new SelectionAdapter() {
 			//0 == Rock, 1 == Paper, 2 == Scissor.
 			//0 beats 2, Loses to 1; 1 Beats 0, Loses to 2; 2 Beats 1, Loses to 0. 
 			public void widgetSelected(SelectionEvent e) {
-				int CompChoice = GenerateRandomChoice();
-				if (CompChoice == 0/*Rock*/) {
-					if(btnUsrRock.getSelection()) {
-						lblWhoWin.setText(tie);
-					} else if(btnUsrPaper.getSelection()) {
-						lblWhoWin.setText(pWin);
-						playerWins++;
-						lblPlayerScore.setText("Player Score :" + playerWins);
-					} else if(btnUsrScissor.getSelection()) {
-						lblWhoWin.setText(cWin);
-						compWins++;
-						lblPlayerScore.setText("Comp's Score : " + compWins);
+				int CompChoice = GenerateRandomChoice(); 			      // Generates a random choice for the computer.
+				if (CompChoice == 0/*Rock*/) {		  			      // If the Computer chooses ROCK...
+					if(btnUsrRock.getSelection()) {  			      // If the player chooses ROCK...
+						lblWhoWin.setText(tie);				      // Tell the user that there was a tie.
+					} else if(btnUsrPaper.getSelection()) { 		      // If the player chooses PAPER...
+						lblWhoWin.setText(pWin);	 		      // Tell the user that they won.
+						playerWins++;			 		      // Increase the player's wins.
+						lblPlayerScore.setText("Player Score :" + playerWins);// Change the text to reflect the total wins. 
+					} else if(btnUsrScissor.getSelection()) {		      // If the Player chooses SCISSOR...
+						lblWhoWin.setText(cWin);	 		      // Tell the user that the computer won.
+						compWins++;			 		      // Increase the computer's wins
+						lblCompScore.setText("Comp's Score : " + compWins);   // Change the text to reflect the total wins.
+					}															
+				} else if (CompChoice == 1/*Paper*/) {  			      // If the Computer chooses PAPER...
+					if(btnUsrRock.getSelection()) {  			      // If the player chooses ROCK...
+						lblWhoWin.setText(cWin);	 		      // Tell the user that the computer won.
+						compWins++;					      // Increase the computer's wins
+						lblCompScore.setText("Comp's Score :" + compWins);    // Change the text to reflect the total wins. 
+					} else if(btnUsrPaper.getSelection()) { 		      // If the player chooses PAPER...
+						lblWhoWin.setText(tie);				      // Tell the user that there was a tie.
+					} else if(btnUsrScissor.getSelection()) {		      // If the Player chooses SCISSOR...
+						lblWhoWin.setText(pWin);	 		       // Tell the user that they won.
+						playerWins++;			 		       // Increase the player's wins.
+						lblPlayerScore.setText("Player Score : " + playerWins);// Change the text to reflect the total wins. 
 					}
-				} else if (CompChoice == 1/*Paper*/) {
-					if(btnUsrRock.getSelection()) {
-						lblWhoWin.setText(cWin);
-						compWins++;
-						lblCompScore.setText("Comp's Score :" + compWins);
-					} else if(btnUsrPaper.getSelection()) {
-						lblWhoWin.setText(tie);
-					} else if(btnUsrScissor.getSelection()) {
-						lblWhoWin.setText(pWin);
-						playerWins++;
-						lblPlayerScore.setText("Player Score : " + playerWins);
-					}
-				} else if (CompChoice == 2/*Scissor*/) {
-					if(btnUsrRock.getSelection()) {
-						lblWhoWin.setText(pWin);
-						playerWins++;
-						lblPlayerScore.setText("Player Score : " + playerWins);
-					} else if(btnUsrPaper.getSelection()) {
-						lblWhoWin.setText(cWin);
-						compWins++;
-						lblCompScore.setText("Comp's Score :" + compWins);
-					} else if(btnUsrScissor.getSelection()) {
-						lblWhoWin.setText(tie);
+				} else if (CompChoice == 2/*Scissor*/) {  			       // If the Computer chooses PAPER...
+					if(btnUsrRock.getSelection()) {  			       // If the player chooses ROCK...
+						lblWhoWin.setText(pWin);	 		       // Tell the user that they won.
+						playerWins++;			 		       // Increase the player's wins.
+						lblPlayerScore.setText("Player Score : " + playerWins);// Change the text to reflect the total wins. 
+					} else if(btnUsrPaper.getSelection()) {   		       // If the player chooses PAPER...
+						lblWhoWin.setText(cWin);	 		       // Tell the user that the computer won.
+						compWins++;			 		       // Increase the computer's wins
+						lblCompScore.setText("Comp's Score :" + compWins);     // Change the text to reflect the total wins. 
+					} else if(btnUsrScissor.getSelection()) {		       // If the Player chooses SCISSOR...
+						lblWhoWin.setText(tie);				       // Tell the user that there was a tie.
 					}
 				}
 				
@@ -179,21 +185,21 @@ public class RockPaperScissors {
 		btnCheck.setText("Check");
 	}
 	
+	//This function will generate a random choice for the computer and return that choice.
 	public int GenerateRandomChoice() {
-		btnCompRock.setSelection(false);
-		btnCompPaper.setSelection(false);
-		btnCompScissor.setSelection(false);
+		btnCompRock.setSelection(false);	  // Set the button selection to false.
+		btnCompPaper.setSelection(false);	  // Set the button selection to false.
+		btnCompScissor.setSelection(false);	  // Set the button selection to false.
 		
-		Random ranNum = new Random(); 
-		int randomInt = ranNum.nextInt(INT_RANGE);
-		if (randomInt == 0) {
-			btnCompRock.setSelection(true);
-		} else if(randomInt == 1) {
-			btnCompPaper.setSelection(true);
-		}else if (randomInt == 2) {
-			btnCompScissor.setSelection(true);
+		Random ranNum = new Random(); 		  // Creates a new Random object.
+		int randomInt = ranNum.nextInt(INT_RANGE);//Generates a random integer
+		if (randomInt == 0) {			  // If ROCK is selected...
+			btnCompRock.setSelection(true);   // Set the button selection to true.
+		} else if(randomInt == 1) {		  // If ROCK is selected...
+			btnCompPaper.setSelection(true);  // Set the button selection to true.
+		}else if (randomInt == 2) {		  // If ROCK is selected...
+			btnCompScissor.setSelection(true);// Set the button selection to true.
 		}
-		
-		return randomInt;
+		return randomInt;			  // Return the chosen selection.
 	}
 }
